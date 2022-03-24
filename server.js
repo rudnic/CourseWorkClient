93a75f07378812d9726542afc58ec35f52049d18
Nikita <werdont@gmail.com>
2022-03-24 15:44:26 +0300

    edited server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/front'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/front/index.html'));
 });
